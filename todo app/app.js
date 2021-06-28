8c1d93624ce714ba3d5e1d355fc7eb547723cc0c
Raza <77836119+Ahmedlathiya@users.noreply.github.com>
2021-06-28 23:25:56 +0500

    new update Todo App

--- todo app/app.js
+++ todo app/app.js
@@ -1,66 +1,99 @@
-let ul = document.getElementById('list'); 
+function toDo() {
 
-function addTodo (){
-    let add = document.getElementById('todo_app');
-    
-    
-/// create li tag 
+    var mainInput = document.getElementById("mainInput")
+    var taskValue = mainInput.value;
 
-    let li  = document.createElement('li');
-    let liText  = document.createTextNode(add.value);
-    li.appendChild(liText)
+
+    if (taskValue == "") {
+        alert("This field cant be empty")
+        return 0
+    }
+    else if (!taskValue.trim().length) {
+        alert('Invalid Task');
+        mainInput.value = ""
+        return 0
+    }
+
+
+
+    var list = document.getElementById("mainList")
+
+
+    var li = document.createElement("li")
+    var inp = document.createElement("input")
+    var btn1 = document.createElement("button")
+    var btn2 = document.createElement("button")
+
+
+
+
+
+    btn1.innerHTML = "EDIT"
+    btn2.innerHTML = "DELETE"
+
+
+    btn1.setAttribute("id", "btn1")
+    btn1.setAttribute("onclick", "updt(this)")
+    btn1.setAttribute("class", " btn btn-success")
+
+    btn2.setAttribute("id", "btn2")
+    btn2.setAttribute("onclick", "del(this.parentElement)")
+    btn2.setAttribute("class", "btn btn-danger ")
+
+    inp.setAttribute("id", "listInput")
+    inp.setAttribute("disabled", "disabled")
+    inp.setAttribute("class", "list-input form-control")
+
+    li.setAttribute("id", "li")
+    li.setAttribute("class", "listitem")
+
     
-    
-/// create delete button    
-    
-    add.value = " "
-    let delet = document.createElement('button');
-    let deleTex = document.createTextNode("Delete")
-    delet.appendChild(deleTex)
-    delet.setAttribute('class','btn')
-    delet.setAttribute('onclick','deleteItem(this)')
-    li.appendChild(delet)
-    
-    
-    /// create Edit button
-    
-    let edit = document.createElement('button');
-    let editText = document.createTextNode('Edit')
-    edit.appendChild(editText)
-    edit.setAttribute('class','btn')
-    edit.setAttribute('onclick','editBtn(this)')
-    
-    
-    li.appendChild(edit)
-    ul.appendChild(li)
+
+
+    list.appendChild(li)
+    li.appendChild(inp)
+    li.appendChild(btn1)
+    li.appendChild(btn2)
+
+
+    inp.value = taskValue;
+    mainInput.value = ""
+
 }
-function deleteItem(e){
-e.parentElement.remove()
+
+
+function reset() {
+    var list = document.getElementById("mainList")
+    list.remove()
+
+    var mainInput = document.getElementById("mainInput")
+    mainInput.value=""
+
+    var ol = document.createElement("ol")
+    ol.setAttribute("id", "mainList")
+
+    var div = document.getElementById("main")
+    div.appendChild(ol)
+
 }
 
-function deleteAll(){
-    list.innerHTML = ""
+
+function updt(btn1) {
+
+    btn1.innerHTML = "UPDATE";
+    btn1.style.backgroundColor = "green";
+    btn1.style.color = "white";
+    btn1.previousSibling.disabled = false
+    btn1.setAttribute("onclick", "innerupdate(this)")
+}
+function innerupdate(btn1) {
+    btn1.innerHTML = "EDIT"
+    btn1.style.backgroundColor = "Yellowgreen";
+    btn1.previousSibling.disabled = true
+    btn1.setAttribute("onclick", "updt(this)")
 
 }
-function editBtn(e){
-let editValue = prompt("Enter Edit Value")
-e.parentElement.firstChild.nodeValue = editValue
+
+function del(data) {
+    data.remove()
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
